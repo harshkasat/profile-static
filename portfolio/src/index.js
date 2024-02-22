80e7ca35c0f6bfb445651fdd15abd486c9d5b556
Harsh Kasat <hkasat@waymore.io>
2024-02-23 02:28:20 +0530

    fixing some change in navbar

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,5 +1,13 @@
-import ReactDOM from 'react-dom/client';
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+import "@fontsource/outfit";
+import "@fontsource/roboto";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
